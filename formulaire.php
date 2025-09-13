2b8e5c7ef1b3b260c9cc562727f7595d58276c96
kamel <fotsopaulin47@proton.me>
2025-09-13 10:01:09 +0200

    ajout des principaux fichiers

--- formulaire.php
+++ formulaire.php
@@ -2,3 +2,16 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Formulaire contact</title>
+  <script src="validation.js" defer></script>
+</head>
+<body>
+  <h1>Formulaire de contact</h1>
+  
+</body>
+</html>
